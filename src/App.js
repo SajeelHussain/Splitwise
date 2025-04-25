8a374f6feb6e06be54bef399e88e631200117aec
Dev <maliksajeelawan68@gmail.com>
2025-04-25 12:08:53 -0700

    Add mock data for users and expenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
-
+import mockData from "./data/mockData";
 function App() {
   return(
     <div className = "container">
       <h1>Splitwise</h1>
-      <button className = "btn btn-primary"></button>
+      <h2>User</h2>
+      <ul>
+        {mockData.users.map((user) =>(
+          <li key = {user.id}>{user.name}</li>
+        ))}
+      </ul>
 
     </div>
   );
---
+++ src/data/mockData.js
@@ -0,0 +1,17 @@
+const mockData = {
+    users: [
+      { id: 1, username: "user1", password: "pass1", name: "Alice" },
+      { id: 2, username: "user2", password: "pass2", name: "Bob" },
+      { id: 3, username: "user3", password: "pass3", name: "Charlie" },
+      { id: 4, username: "user4", password: "pass4", name: "David" },
+      { id: 5, username: "user5", password: "pass5", name: "Eve" },
+      { id: 6, username: "user6", password: "pass6", name: "Frank" },
+      { id: 7, username: "user7", password: "pass7", name: "Grace" },
+      { id: 8, username: "user8", password: "pass8", name: "Hannah" },
+      { id: 9, username: "user9", password: "pass9", name: "Ian" },
+      { id: 10, username: "user10", password: "pass10", name: "Jane" },
+    ],
+    expenses: [],
+  };
+  
+  export default mockData;
